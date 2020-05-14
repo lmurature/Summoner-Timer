257bcf328c00413305841d48c7c0646528c3e977
lmurature <lucio.murature@mercadolibre.com>
2020-05-14 17:27:05 -0300

    Initial Commit

---
+++ Summoner Timer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Summoner Timer
+//
+//  Created by Lucio Murature on 14/05/2020.
+//  Copyright © 2020 Lucio Murature. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Summoner TimerTests/Summoner_TimerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Summoner_TimerTests.swift
+//  Summoner TimerTests
+//
+//  Created by Lucio Murature on 14/05/2020.
+//  Copyright © 2020 Lucio Murature. All rights reserved.
+//
+
+import XCTest
+@testable import Summoner_Timer
+
+class Summoner_TimerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
